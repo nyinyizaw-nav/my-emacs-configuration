b536e1cdc2c03590c2256e98cfb2c39514587d27
nyinyizaw-nav <nyinyizaw.nav@gmail.com>
2022-05-20 23:38:59 +0630

    Disable beep sound

--- init.el
+++ init.el
@@ -4,29 +4,30 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
- ;; Define and initialise package repositories
-  (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (package-initialize)
+;; Define and initialise package repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-  ;; use-package to simplify the config file
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
+;; use-package to simplify the config file
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+   (package-install 'use-package))
   (require 'use-package)
 (setq use-package-always-ensure 't)
 
-  ;; Keyboard-centric user interface
-  (setq inhibit-startup-message t)
-  (tool-bar-mode -1)
-  (menu-bar-mode -1)
-  (scroll-bar-mode -1)
+;; Keyboard-centric user interface
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-  ;; Theme
-  (use-package exotica-theme
-    :config (load-theme 'exotica t))
+;; Theme
+(use-package exotica-theme
+  :config (load-theme 'exotica t))
 
 (set-face-attribute 'default nil :height 160)
 
-
+;; Disable bell sound
+(setq visible-bell 1)
